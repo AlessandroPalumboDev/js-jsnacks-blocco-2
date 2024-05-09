6901db32e55d083f84d520e871181059f8a48bfe
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-09 09:47:35 +0200

    [FIX] snack-b corretto e funzionante

--- snack-b/js/main.js
+++ snack-b/js/main.js
@@ -4,30 +4,35 @@
 const listaElementi = ['uno','due','tre','quattro','cinque','sei','sette','otto'];
 
 // creo un prompt che salvo in una variabile
-const input = prompt("Digita l' elemento da cercare nella lista");
+const input = prompt("Digita l' elemento da cercare nella lista").toLowerCase();
 
 // creo la funzione trovaIndice
 function trovaIndice(trova, elemento){
     // creo la variabile per la posizione
     let posizione = 0;
 
+    // dichiaro variabile booleana su falso
+    let check = false;
+
     // creo un ciclo for che cicla per tutti gli elementi finché non trova l'elemento'
     for (let i = 0; i < trova.length; i++){
 
-        // 
+        // se elemento inserito è uguale a elemento array
         if(elemento === trova[i]){
+            check = true;
             posizione = i + 1;
-
-        //  
-        }else{
-            posizione = (`-1 Perchè non in lista`);
         };
+    };
+
+    // FUORI DAL CICLO !
+    //  quindi se check è falso la posizione risulta -1
+    if(check === false){
+        posizione = (`-1 Perchè non in lista`);
     };
 
     // restituisce la posizione
     return posizione;
 };
 
-alert(`L'elemento "${input}" richiesto si trova in posizione ${trovaIndice(listaElementi)}`);
+alert(`L'elemento "${input}" richiesto si trova in posizione ${trovaIndice(listaElementi, input)}`);
 
-// per ora non funziona 
