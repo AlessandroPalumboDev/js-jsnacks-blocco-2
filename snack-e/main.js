1f50a0a64188abb1b599805f22e656a4a4cbb6eb
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-12 18:44:03 +0200

    [UPD] snack-e to main.js

--- snack-e/main.js
+++ snack-e/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Creo un array di input
+const arrayInput = [1, 2, 3, 4, 5,];
+
+// Creo un array di oputput vuoto
+const arrayOutput = [];
+
+// Definisco la funzione rimuoviDallaTesta(arrInput, arrOutput)
+function rimuoviDallaTesta(input, output){
+
+    // Creo un ciclo for con i che parte da 1(quindi la seconda posizione dell array di input)
+    for(let i = 1; i < input.length; i++){
+        // l'elemento i - 1 dell'array di output è uguale all'elemento i dell' array di input
+        output[i-1] = input[i];
+    }
+
+    // return dell'array di oputput
+    return output;
+};
+
+// Stampo in console invocando la funzione
+console.log(rimuoviDallaTesta(arrayInput, arrayOutput));
